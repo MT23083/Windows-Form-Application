276c4df0956ecaf19eb4d01ee240c3606950e22b
Drishti <drishti.singh207@gmail.com>
2024-06-21 17:38:56 +0530

    Initial Commit

---
+++ FormApplication/FormApplication/SubmissionEntry.vb
@@ -0,0 +1,13 @@
+﻿Public Class SubmissionEntry
+    Public Property Name As String
+    Public Property Email As String
+    Public Property Phone As String
+    Public Property GitHub As String
+
+    Public Sub New(name As String, email As String, phone As String, github As String)
+        Me.Name = name
+        Me.Email = email
+        Me.Phone = phone
+        Me.GitHub = github
+    End Sub
+End Class
